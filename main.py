f739df6b4f7cbe9d6047337c86b52a8692880c0a
hadargabbay <123993899+hadargabbay@users.noreply.github.com>
2024-01-21 19:38:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
+from flask import Flask, jsonify
 import requests
 import time
 
+app = Flask(__name__)
 
-def fetch_bitcoin_value():
-    api_url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+current_btc_price = None
+avg_price = None
+previous_prices = []
 
+
+# Function to fetch Bitcoin price from CoinGecko API
+def fetch_bitcoin_price(print_output=True):
     try:
-        response = requests.get(api_url)
-        response.raise_for_status()  # Check for HTTP errors
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+        response = requests.get(url)
+        data = response.json()
 
-        bitcoin_value = response.json()['bitcoin']['usd']
-        return bitcoin_value
-    except Exception as e:
-        print(f"Error fetching Bitcoin value: {e}")
-        return None
+        if "error" in data:
+            print(f"Error fetching Bitcoin price: {data['error']['message']}")
+            return False
+
+        bitcoin_price = data["bitcoin"]["usd"]  # Bitcoin price in USD
+        if print_output:
+            print(f"Bitcoin price: ${bitcoin_price}")
+
+        global current_btc_price
+        current_btc_price = bitcoin_price
+
+        if print_output:
+            previous_prices.append(current_btc_price)
+
+        if len(previous_prices) == 10:
+            global avg_price
+            avg_price = sum(previous_prices) / 10
+            if print_output:
+                print(f"Average price for the past 10 minutes: {avg_price}")
+            previous_prices.clear()
+
+        return True
+    except Exception as error:
+        print(f"Error fetching Bitcoin price: {error}")
+        return False
 
 
-def main():
-    values = []
-
-    while True:
-        bitcoin_value = fetch_bitcoin_value()
-
-        if bitcoin_value is not None:
-            print(f"Bitcoin Value: {bitcoin_value} USD")
-            values.append(bitcoin_value)
-
-            if len(values) % 10 == 0:
-                average_value = sum(values) / len(values)
-                print(f"Average Value (Last 10 minutes): {average_value} USD")
-                values = []  # Reset values list for the next 10 minutes
-
-        time.sleep(60)  # Sleep for 1 minute
+@app.route('/service-a/healthz')
+def health_check():
+    return 'OK', 200
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/service-a/readiness')
+def readiness_check():
+    can_fetch = fetch_bitcoin_price(False)
+    return ('OK', 200) if can_fetch else ('ERROR', 400)
+
+
+@app.route('/service-a')
+def get_btc_data():
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+    return jsonify({'Time': now, 'Price': current_btc_price, 'Average_Price': avg_price})
+
+
+fetch_bitcoin_price()
+while True:
+    time.sleep(60)
+    fetch_bitcoin_price()
+
+if __name__ == '__main__':
+    app.run(port=80)
